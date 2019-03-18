1d2878d956cc04b9ffe9427dd7ee2972c22a399c
ABISEK-K-S <39756233+ABISEK-K-S@users.noreply.github.com>
2019-03-18 12:01:29 +0530

    Add files via upload

---
+++ staff/civil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+
+ <?php
+$servername="localhost";
+$username="root";
+$password="";
+$db="timetable";
+$conn = new mysqli($servername, $username, $password, $db);
+$a=$_POST['mark1'];
+$b=$_POST['mark2'];
+$c=$_POST['mark3'];
+$d=$_POST['roll'];
+        
+		$sql = "insert into civil(m1,m2,m3,roll_no) VALUES('$a','$b','$c','$d')";
+    
+        
+if ($conn->query($sql)==TRUE)
+{
+ echo "Updated   Success";
+} 
+else
+{
+    echo "  user not available";
+}
+
+$conn->close();
+?>
+</div>
+</body>
+</html>
